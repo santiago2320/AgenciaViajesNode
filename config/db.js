064a42748528b0ef4228395f2b5f4c63b5173549
santiago2320 <santiagolope373@gmail.com>
2023-10-24 13:25:09 +0200

    Correcion en la variable DB_HOST en el archivo db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ console.log(process.env.DB_HOST);
 
 
 const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
-    host:process.env.HOST,
+    host:process.env.DB_HOST,
     port: '3306',
     dialect: 'mysql',
     define:{
